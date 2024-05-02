aa6df6c851639268fb0342c436f81d33e80137ae
PabloBarresi <pablojavierba@gmail.com>
2024-05-02 17:37:18 -0300

    Cambios Semana 6

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,37 +1,120 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MvcMovie.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MvcMovie.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public async Task<IActionResult> Index()
+        private static List<Movie> _movies = new List<Movie>();
+
+        // GET: Movie
+        public IActionResult Index()
         {
-            var listMovies = new List<Movie>();
+            return View(_movies);
+        }
 
-            var movie1 = new Movie
+        // GET: Movie/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
             {
-                Genre = "Terror",
-                Id = 1,
-                Price = 1,
-                ReleaseDate = DateTime.Now,
-                Title = "La noche del terror"
-            };
-            listMovies.Add(movie1);
+                return NotFound();
+            }
 
-            var movie2 = new Movie
+            var movie = _movies.Find(m => m.Id == id);
+            if (movie == null)
             {
-                Genre = "Terror",
-                Id = 1,
-                Price = 1,
-                ReleaseDate = DateTime.Now,
-                Title = "La noche del terror II"
-            };
-            listMovies.Add(movie2);
+                return NotFound();
+            }
 
-            return View(listMovies);
+            return View(movie);
+        }
 
+        // GET: Movie/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movie/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _movies.Add(movie);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movie/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = _movies.Find(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movie/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var existingMovie = _movies.Find(m => m.Id == id);
+                existingMovie.Title = movie.Title;
+                existingMovie.ReleaseDate = movie.ReleaseDate;
+                existingMovie.Genre = movie.Genre;
+                existingMovie.Price = movie.Price;
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movie/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = _movies.Find(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movie/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var movie = _movies.Find(m => m.Id == id);
+            _movies.Remove(movie);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
